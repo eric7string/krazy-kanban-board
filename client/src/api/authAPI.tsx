8447f811e31360d00547c48f727c60f48d11fbb9
eric7string <153536554+eric7string@users.noreply.github.com>
2024-10-19 14:12:04 -0400

    updated client side authApi

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,7 +2,27 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error('Error logging in, view network tab');
+    }
+
+    return data;
+  } catch (error) {
+    console.log('Error logging in', error);
+    return Promise.reject('Error logging in');
+  }
+};
 
 
 
